0fdaf2d75a3a8b17253ce2d078ec349493a6f5f3
Abdallah <abdamoh1@gmail.com>
2021-06-08 08:55:02 +0530

    this is second test

--- src/com/riigsoft/Main.java
+++ src/com/riigsoft/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World IJ by using git commands");
+        System.out.println("Hello World IJ by using git commands");
     }
 }
